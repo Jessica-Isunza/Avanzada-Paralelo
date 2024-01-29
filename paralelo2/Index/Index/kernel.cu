ff2c3a196bc96e153705db1f3398b666000994b9
Jessica-Isunza <145494578+Jessica-Isunza@users.noreply.github.com>
2024-01-29 09:58:29 -0600

    Index
    primer codigo

---
+++ paralelo2/Index/Index/kernel.cu
@@ -0,0 +1,17 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void print_hello_cuda()
+{
+    int i = threadIdx.x;
+    printf("[DEVICE] ThreadIdx: %d",i);
+}
+
+int main()
+{
+    print_hello_cuda << <1, 8 >> > ();
+    return 0;
+}
